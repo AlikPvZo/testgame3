e31a109cae391573e6d03205ec621280f551b0c7
AlikPvZo <caroslav768@gmail.com>
2025-05-03 12:05:58 +0300

    add3objct

---
+++ script.js
@@ -0,0 +1,7 @@
+// Пример: приветствие при загрузке страницы
+alert("Добро пожаловать на мой сайт!");
+
+// Пример: изменение цвета заголовка при клике
+document.querySelector("h1").addEventListener("click", function() {
+    this.style.color = "red";
+});
